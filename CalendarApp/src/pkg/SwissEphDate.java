3870a17461f21a065926058da10bcaad306b27a7
Ropert21 <59937051+Ropert21@users.noreply.github.com>
2021-10-13 19:44:23 -0700

    More features added, almost complete
    Added sunset, sunrise, lunar eclipse, solar eclipse and various other things that will be useful for creating the gui

--- CalendarApp/src/pkg/SwissEphDate.java
+++ CalendarApp/src/pkg/SwissEphDate.java
@@ -1,6 +1,8 @@
 package pkg;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+
 import swisseph.*;
 
 /*
@@ -17,7 +19,6 @@ public class SwissEphDate {
 	SwissEph sw = new SwissEph();
 
 	// Setting up constants which will be used with sw when performing calculations
-	// later
 	final int EARTH = SweConst.SE_EARTH;
 	final int MOON = SweConst.SE_MOON;
 	final int SUN = SweConst.SE_SUN;
@@ -27,7 +28,7 @@ public class SwissEphDate {
 	LocalDate gregDate;
 	double julianDate;
 	double[] position; // contains longitude, latitude and elevation in that order
-	double[] data, tret, attr = new double[20]; // used for holding data from calculations
+	int timeZoneOffset;
 
 	public SwissEphDate(LocalDate date, double longitude, double latitude, double elevation) {
 		// Setting up sd with date provided
@@ -38,11 +39,22 @@ public class SwissEphDate {
 
 		// Creating position array with data provided
 		position = new double[] { longitude, latitude, elevation };
+		
+		// Calculates time zone from longitude 
+		timeZoneOffset = (int)(longitude / 15) * -1;
+		
+		// Puts sd object in gregorian mode to make converting from julian easier later
+		sd.setCalendarType(true, true);
 	}
 
 	
 	// *** Get methods for variables that may be useful
 	public double getJulianDate() { return julianDate; }
+	public int getYear() { return gregDate.getYear(); }
+	public int getMonth() { return gregDate.getMonthValue(); }
+	public int getDayOfMonth() { return gregDate.getDayOfMonth(); }
+	public int getDayOfWeek() { return gregDate.getDayOfWeek().getValue(); }
+	public int getTimeZone() { return timeZoneOffset; } // returns amount of hours ahead / behind greenwich time
 
 	// *** Main calculation methods (moon, sunrise/set, eclipses, ...)
 
@@ -72,46 +84,84 @@ public class SwissEphDate {
 	
 	// Returns % of moon lit relative to earth, useful for moon phase calculations
 	public double getMoonPhasePercent() {
-		// Calculates % of moon currently lit relative to earth into data[0]
-		sw.swe_pheno(julianDate, MOON, SweConst.SEFLG_SWIEPH, data, new StringBuffer("Calculation failed"));
-		return data[0];
+		// Calculates % of moon currently lit relative to earth into data[1]
+		double[] data = new double[20];
+		sw.swe_pheno(julianDate, MOON, SWISSEPH, data, new StringBuffer("Calculation failed"));
+		return data[1];
 	}
-
-	/*
-	 * public String getSunriseTime() {
-	 * 
-	 * }
-	 * 
-	 * public String getSunsetTime() {
-	 * 
-	 * }
-	 * 
-	 * public boolean isSolarEclipse() {
-	 * 
-	 * }
-	 * 
-	 * public boolean isLunarEclipse() {
-	 * 
-	 * }
-	 */
+	
+	// Returns a string of the sunrise time in 24 hours for current date eg. 18:32 or 6:07
+	public String getSunriseTime() {
+		DblObj data = new DblObj(0); // Holds julian date of when sunrise occurs after calculation
+		sw.swe_rise_trans(julianDate, SUN, null, SWISSEPH, SweConst.SE_CALC_RISE, position, 0, 22, data, new StringBuffer("Sunrise Calculation failed"));
+		
+		// Converting julian date provided to time
+		sd.setJulDay(data.val); 
+		int hour = setTimezone(sd.getHour());
+		return (hour + ":" + getMinutes(sd.getHour()));
+	}
+	
+	// Same as above but for sunset
+	public String getSunsetTime() {
+		DblObj data = new DblObj(0); // Holds julian date of when sunrise occurs after calculation
+		sw.swe_rise_trans(julianDate, SUN, null, SWISSEPH, SweConst.SE_CALC_SET, position, 0, 22, data, new StringBuffer("Sunset Calculation failed"));
+		
+		// Converting julian date provided to time
+		sd.setJulDay(data.val); 
+		int hour = setTimezone(sd.getHour());
+		return (hour + ":" + getMinutes(sd.getHour()));
+	}
+	
+	// Returns a LocalDate object containing the next solar eclipse for provided location
+	public LocalDate getNextSolarEclipse() {
+		double[] tret = new double[20], attr = new double[20]; // used for holding data from calculations
+		sw.swe_sol_eclipse_when_loc(julianDate, SWISSEPH, position, tret, attr, 0, new StringBuffer("s eclipse Calculation failed"));
+		
+		sd.setJulDay(tret[0]); // Calculation puts julian date of occurance in tret[0]
+		return LocalDate.of(sd.getYear(), sd.getMonth(), sd.getDay());
+	}
+	
+	public LocalDate getNextLunarEclipse() {
+		double[] tret = new double[20], attr = new double[20]; // used for holding data from calculations
+		sw.swe_lun_eclipse_when_loc(julianDate, SWISSEPH, position, tret, attr, 0, new StringBuffer("Machine broke"));
+		
+		sd.setJulDay(tret[0]); // Calculation puts julian date of occurance in tret[0]
+		return LocalDate.of(sd.getYear(), sd.getMonth(), sd.getDay());
+	}
+	
 	// *** Other calculation methods that might be useful for a GUI
 	
 	// Returns another SwissEphDate object + or - the amount of days specified in the method
 	public SwissEphDate anotherDate(int days) {
 		return new SwissEphDate(gregDate.plusDays(days), position[0], position[1], position[2]);
 	}
+	
+	// Returns amount of days of the month the date is in
+	public int daysInMonth() {
+		int current = gregDate.getDayOfMonth();
+		int next = gregDate.getDayOfMonth();
+		int inc = 1;
+		
+		while (next != 1) {
+			current = next;
+			next = anotherDate(inc).getDayOfMonth();
+			inc ++;
+		}
+		
+		return current;
+	}
 
-		// *** Private methods to help with calculations
+	// *** Private methods to help with calculations
 
-	// Sets an hour output to a time zone (in our case PST is +7)
-	public int setTimezone(double input, int timedifference) {
-		int output = (int) input - timedifference;
+	// Sets an hour output to a time zone
+	private int setTimezone(double input) {
+		int output = (int) input - timeZoneOffset;
 		if (output < 0) output += 24;
 		return output;
 	}
 
 	// Gets number of minutes from the fraction on the sd.getHour() method
-	public String getMinutes(double input) {
+	private String getMinutes(double input) {
 		int minutes = (int) ((input - (int) input) * 60);
 		String output = Integer.toString(minutes);
 		if (minutes < 10)
--- CalendarApp/src/pkg/SwissEphDateTester.java
+++ CalendarApp/src/pkg/SwissEphDateTester.java
@@ -1,9 +1,55 @@
 package pkg;
 
+import java.time.LocalDate;
+
 public class SwissEphDateTester {
 	public static void main(String[] args) {
-		SwissEphDate s;
+		SwissEphDate s = new SwissEphDate(LocalDate.of(2021, 10, 13), -119.4960, 49.803, 334.0);
 		
+		// Julian to gregorian test
+		if(s.getJulianDate() != 2459500.5)
+			System.out.println("test 1 failed");
 		
+		// Get day of week test
+		if(s.getDayOfWeek() != 3)
+			System.out.println("test 2 failed");
+		
+		// Another day test
+		if(s.anotherDate(1).getDayOfWeek() != 4)
+			System.out.println("test 3 failed");
+		
+		// Timezone test
+		if(s.getTimeZone() != 7)
+			System.out.println("test 4 failed");
+		
+		// Moon % test
+		if(s.getMoonPhasePercent() != 0.48514204801618566)
+			System.out.println("test 5 failed");
+		
+		// Moon phase test
+		if(!s.getMoonPhase().contentEquals("Waxing Crescent"))
+			System.out.println("test 6 failed");
+		
+		// Sunrise time test
+		if(!s.getSunriseTime().contentEquals("7:17"))
+			System.out.println("test 7 failed");
+		
+		// Sunset time test
+		if(!s.getSunsetTime().contentEquals("18:11"))
+			System.out.println("test 8 failed");
+		
+		// Solar eclipse test
+		if(!s.getNextSolarEclipse().equals(LocalDate.of(2023, 10, 14)))
+			System.out.println("test 9 failed");
+		
+		// Lunar eclipse test
+		if(!s.getNextLunarEclipse().equals(LocalDate.of(2021, 11, 19)))
+			System.out.println("test 10 failed");
+		
+		// Days in month test
+		if(s.daysInMonth() != 31)
+			System.out.println("test 11 failed");
+		
+		System.out.println("tests completed.");
 	}
 }
